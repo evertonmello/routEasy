afda7c11635b85b092515365cacf47c2a5e113b7
Everton Mello <everton.mello@engdb.com.br>
2017-09-22 18:27:51 -0300

    -

--- db/mongoose.js
+++ db/mongoose.js
@@ -7,7 +7,7 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('conectado no banco' + config.db);
+  console.log('conectado no banco ' + config.db);
 });
 
 module.exports = mongoose;
